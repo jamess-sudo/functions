34ced166028cd5356769cba227328d90cb091af9
jamess-sudo <aj369369aj@gmail.com>
2026-03-23 12:58:27 +0400

    Add files via upload

---
+++ sade_def_kalkulyator_inputsuz.py
@@ -0,0 +1,24 @@
+def topla(x,y):
+    return x+y
+
+toplama=topla(5,2)
+
+def cixma(x,y):
+    return x-y
+
+cixilma=cixma(5,2)
+
+def vurma(x,y):
+    return x*y
+
+vurulma=vurma(5,2)
+
+def bolme(x,y):
+    return x/y
+
+bolunme=bolme(5,2)
+
+print(f"Ededlerinizin cemi: {toplama}")
+print(f"Ededlerinizin ferqi: {cixilma}")
+print(f"Ededlerinizin hasili: {vurulma}")
+print(f"Ededlerinizin qismeti: {bolunme}")
